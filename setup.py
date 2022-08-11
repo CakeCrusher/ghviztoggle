bdcb0b9f8f3264caf1222da6447bf4e758a7972e
CakeCrusher <1sebastian1sosa1@gmail.com>
2022-08-11 17:02:29 -0400

    .

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ setup(
     name='ghviztoggle',
     version='1.0.0',
     description='Quickly toggle the visibility of your GitHub repositories.',
-    url='https://github.com/CakeCrusher/mimicbot/tree/master/mimicbot_cli/mimicbot_chat',
+    url='https://github.com/CakeCrusher/ghviztoggle',
     author='Sebastian Sosa',
     author_email='1sebastian1sosa1@gmail.com',
-    license='MIT License',
     packages=['ghviztoggle'],
     install_requires=[
         'requests',
@@ -16,7 +15,6 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.10',
     ],
 )
